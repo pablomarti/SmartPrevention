594163f665149d28ce27ddc2366d0c64f1811ffa
Moises Portillo <moises.gilberto@gmail.com>
2013-10-05 05:06:14 -0600

    update

--- src/com/example/smartprevention/MainActivity.java
+++ src/com/example/smartprevention/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.smartprevention;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-
+	public ImageButton alert_critic_button,sos_button, save_button,unknow_button; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
